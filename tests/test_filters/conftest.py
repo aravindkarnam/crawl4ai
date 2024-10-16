2c24b309fae7ca6455c4010943d7584898c59a18
Agaba Edwin <agabsed@gmail.com>
2024-10-16 04:00:49 -0700

    Temporarily remove test files for release

--- tests/test_filters/conftest.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-
-# Add the parent directory to the Python path
-parent_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.path.append(parent_dir)
